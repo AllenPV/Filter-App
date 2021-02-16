b96bfbe078ddc48e982c8f3de1198a3f838d60b3
AllenPV <73430174+AllenPV@users.noreply.github.com>
2021-02-16 11:57:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,67 +1,21 @@
+function preload(){
 
-
-    function setup(){
-        Canvas = createCanvas(640,480);
-        Canvas.position(50,150);
-        Video = createCapture(VIDEO);
-        Video.hide();
+}
+function setup(){
+    Canvas = createCanvas(640,480);
+    Canvas.position(110,250);
+    Video = createCapture(VIDEO);
+    Video.hide();
+    tint_color = "";
 }
 function draw(){
-    image(Video,170,150,220,200);
-   
-
-   fill(255,255,0);
-   stroke(255,255,0);
-   rect(40,20,470,50)
-
-   fill(255,0,0);
-   stroke(255,0,0);
-   circle(50,50,80);
-   
-   fill(255,0,0);
-   stroke(255,0,0);
-   circle(520,50,80);
-
-   fill(255,255,0);
-   stroke(255,255,0);
-   rect(40,403,470,50);
-   
-   
-
-   fill(255,0,0);
-   stroke(255,0,0);
-   circle(520,430,80);
-
-   fill(255,0,0);
-   stroke(255,0,0);
-   circle(50,430,80);
-
-   fill(255,255,0);
-   stroke(255,255,0);
-   rect(45,28,50,420);
-
-   fill(255,255,0);
-   stroke(255,255,0);
-   rect(450,28,50,420);
-  
+    image(Video,0,0,640,480);
+    tint(tint_color);
 }
-  
-
-  
-
-   
-
-   
-
-  
-
-   
-
-   
-
-   
-
-
-   
-   
-   
+function take_snapshot()
+{
+save("Output.png");
+}
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
